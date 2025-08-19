ee009cc5e6c1582cba23b13ba348025802ed1b51
M. RIVIERE <123306167+MaddyNozor@users.noreply.github.com>
2025-08-19 18:14:31 +0000

    megaphpne fonctionnel

--- 0_/ex00/main.cpp
+++ 0_/ex00/main.cpp
@@ -11,23 +11,54 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <locale>
 
 # define SUCCESS 0;
 
+char    *scream_this(char *str)
+{
+    int j = 0;
+
+    while(str[j])
+    {
+        str[j] = std::toupper(str[j]);
+        j++;
+    }
+    return (str);
+}
+
 int     main(int ac, char **av)
 {
-    int i = 1;
+    int i;
 
     if (ac == 1)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return SUCCESS;
     }
+    i = 1;
     while(av[i])
     {
-        std::cout << av[i] << " ";
-        i++;
+        av[i] = scream_this(av[i]);
+        std::cout << av[i++];
     }
     std::cout << std::endl;
     return SUCCESS;
 }
+
+/*    
+    i = 1;
+    while(av[i])
+    {
+        j = 0;
+        while(av[i][j])
+        {
+            av[i][j] = std::toupper(av[i][j]);
+            j++;
+        }
+        std::cout << av[i++];
+    }
+    std::cout << std::endl;
+    return SUCCESS;
+} 
+*/
