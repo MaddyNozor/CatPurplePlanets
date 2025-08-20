ad880205c377abce7412c221bf66a1a293faf7b5
M. RIVIERE <123306167+MaddyNozor@users.noreply.github.com>
2025-08-20 20:12:30 +0000

    start to build contact

--- 0_/ex01/_Contact.cpp
+++ 0_/ex01/_Contact.cpp
@@ -4,9 +4,10 @@
 Contact::Contact(void)
 {
     std::cout << "Call Contact Constructor" << std::endl;
+    this->id = 42;
 }
 
 Contact::~Contact(void)
 {
-    std::cout << ". ";
+    std::cout << "Destroy Contact " << this->id << std::endl;
 }
--- 0_/ex01/_Contact.hpp
+++ 0_/ex01/_Contact.hpp
@@ -3,8 +3,14 @@
 
 class Contact
 {
+    private:
+        int         id;
+        std::string firstname;
+        std::string lastname;
+        std::string nickname;
+        std::string phone_number;
+        std::string darkest_secret;
     public:
-
-    Contact(void);
-    ~Contact(void);
+        Contact(void);
+        ~Contact(void);
 };
