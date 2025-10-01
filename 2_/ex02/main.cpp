f38a101b27a63b3fcec14874140c57eae96d71e9
Mailys RIVIERE <mailys.riviere@gmail.com>
2025-10-01 13:06:05 +0200

    add main with check arg

--- 2_/ex02/main.cpp
+++ 2_/ex02/main.cpp
@@ -6,36 +6,51 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 17:05:15 by mairivie          #+#    #+#             */
-/*   Updated: 2025/09/11 19:40:10 by mairivie         ###   ########.fr       */
+/*   Updated: 2025/10/01 13:04:25 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
+#include <cstdlib>
 
-int main( void ) {
-Fixed a;
-Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-std::cout << a << std::endl;
-std::cout << ++a << std::endl;
-std::cout << a << std::endl;
-std::cout << a++ << std::endl;
-std::cout << a << std::endl;
-std::cout << b << std::endl;
-std::cout << Fixed::max( a, b ) << std::endl;
-return 0;
+// int main( void ) {
+// Fixed a;
+// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+// std::cout << a << std::endl;
+// std::cout << ++a << std::endl;
+// std::cout << a << std::endl;
+// std::cout << a++ << std::endl;
+// std::cout << a << std::endl;
+// std::cout << b << std::endl;
+// std::cout << Fixed::max( a, b ) << std::endl;
+// return 0;
+// }
+
+#define INPUT_ERROR "Invalid input argument !\n Format is ./fixed [int] [float] [float]\n"
+
+int checkFloatArgument(int ac, char** av) {
+   if (ac != 4)
+      return EXIT_FAIL;
+   if (atoi(av[1]) == 0)
+      return EXIT_FAIL;
+   if (atof(av[2]) == 0 || atof(av[3]) == 0)
+      return EXIT_FAIL;
+   return EXIT_SUCCESS;
 }
 
-//TODO: check le fait qu'on doive tranformer des nb en string (only string on cout)
-/*
-std::string toString(const int nb)
-{
-   std::ostringstream oss;
-   oss << nb;
-   return oss.str();
+int main(int ac, char** av) {
+   if (checkFloatArgument(ac, av) == EXIT_FAIL) {
+      std::cout << RED << INPUT_ERROR << RESET << std::endl;
+      return EXIT_FAIL;
+   }
+   int i = atoi(av[1]);
+   float f = atof(av[2]);
+   float g = atof(av[3]);
+
+   std::cout << i << std::endl;
+   std::cout << f << std::endl;
+   std::cout << g << std::endl;
+
+   return EXIT_SUCCESS;
 }
-int nombre = 5;
-string texte = "nombre : " + toString(nombre);
- 
-cout << texte << endl;
-*/
