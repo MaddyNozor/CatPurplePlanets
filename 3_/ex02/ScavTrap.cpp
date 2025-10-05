58439a45e5df7f5c1e4b4da1bb0c4f25faa8b3ab
M. RIVIERE <123306167+MaddyNozor@users.noreply.github.com>
2025-10-05 18:07:37 +0000

    Copy ScavTrap files in ex02

---
+++ 3_/ex02/ScavTrap.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 14:26:57 by mairivie          #+#    #+#             */
+/*   Updated: 2025/10/03 14:28:55 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() {
+    this->setHitPoint(MAX_HP_ST);
+    setManaPoint(MAX_MANA_ST);
+    setAttackDamage(20);
+    setName("[ANONYMOUS]");
+    std::cout << BLUE << "Call ScavTrap constructor with no name." << RESET << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) {
+    this->setHitPoint(MAX_HP_ST);
+    setManaPoint(MAX_MANA_ST);
+    setAttackDamage(20);
+    if (name == "") {
+        setName("[ANONYMOUS]");
+        std::cout << BLUE << "Call ScavTrap constructor with empty name" << RESET << std::endl;
+        return;
+    }
+    setName(name);
+    std::cout << BLUE << "Call ScavTrap constructor with name." << RESET << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &toCopy) : ClapTrap(toCopy) {
+    *this = toCopy;
+    std::cout << BLUE << "Call ScavTrap copy function." << RESET << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &toCopy) {
+    if(this != &toCopy) {
+        this->setName(toCopy.getName());
+        this->setHitPoint(toCopy.getHitPoint());
+        setManaPoint(toCopy.getManaPoint());
+        setAttackDamage(toCopy.getAttackDamage());
+    }
+    return *this;
+}
+
+ScavTrap::~ScavTrap() {
+std::cout << BLUE << "Call Scav Trap destructor." << RESET << std::endl;
+}
+
+// Input: valid target's Name
+// Return: Nothing
+// Effect: explicit what's happenning when ClapTrap attacks
+void    ScavTrap::attack(const std::string& target) {
+    if (getName() == "[ANONYMOUS]"){
+        std::cout << RED << "Error : ScavTrap couldn't find the target" << RESET << std::endl;
+        return;
+    }
+    if (getManaPoint() <= 0){
+        std::cout << RED << "Error : ScavTrap " << this->getName() << " needs more Mana" << RESET << std::endl;
+        return;
+    }
+    this->setManaPoint(this->getManaPoint() - 1);
+    std::cout << CYAN << "ScavTrap " << this->getName() << " attacks " << target << ", causing " << this->getAttackDamage() << " points of damage !" << RESET << std::endl;
+    return;
+}
+
+// Input: none
+// Return: none
+// Effect: Info, ScavTrap is in guard mode.
+void ScavTrap::guardGate() {
+	std::cout << GREEN "ScavTrap " << getName() << " is in GATE KEEPER mode" RESET << std::endl;
+}
---
+++ 3_/ex02/ScavTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 14:27:01 by mairivie          #+#    #+#             */
+/*   Updated: 2025/10/03 14:33:40 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ScavTrap_HPP
+#define ScavTrap_HPP
+
+// ===== INCLUDES =====
+#include <iostream>
+#include <string>
+#include "colorsTools.hpp"
+#include "ClapTrap.hpp"
+
+// ===== DEFINE CLEAN CODE =====
+#define EXIT_SUCCESS 0
+#define EXIT_FAIL 1
+#define MAX_HP_CT 10
+#define MAX_MANA_CT 10
+#define MAX_HP_ST 100
+#define MAX_MANA_ST 50
+
+// ===== CLASS DECLARATION =====
+
+class ScavTrap : public ClapTrap{
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& toCopy);
+		~ScavTrap();
+
+		ScavTrap& operator=(const ScavTrap& toCopy);
+	
+		void guardGate();
+		void attack(const std::string& target);
+};
+
+//See ? No new setters or getters here
+//I can still use them (ClapTrap's) in .cpp
+
+#endif
